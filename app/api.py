97f097ff39cd2e294f6318433d9028940d0deb27
Zachary Yaro <zmyaro@gmail.com>
2015-05-18 23:34:00 -0400

    Implement retrieving playlists and their songs

--- app/api.py
+++ app/api.py
@@ -12,38 +12,78 @@ from google.appengine.ext.webapp.util import run_wsgi_app
 
 from models import Playlist, PlaylistUser, PlaylistItem
 
-def error404(response):
-	response.headers['Content-Type'] = 'text/plain'
-	response.out.write('Error 404')
-	response.set_status(404)
+def errorOut(response, code):
+	response.headers['Content-Type'] = 'application/json'
+	response.out.write('[]')
+	response.set_status(code)
 
 class PlaylistsHandler(webapp.RequestHandler):
 	def get(self):
-		self.response.headers['Content-Type'] = 'application/json'
 		
 		user = users.get_current_user()
 		
-		playlistData = []
+		if not user:
+			errorOut(self.response, 403)
+			return
 		
-		if user:
-			playlistUserMaps = PlaylistUser.gql('WHERE user = :1', user).fetch(limit=None)
-			for playlistUserMap in playlistUserMaps:
-				playlist = Playlist.gql('WHERE playlistId = :1', playlistUserMap.playlistId).get()
-				playlistData.append({
-					'playlistId': playlist.playlistId,
-					'name': playlist.name
-				})
-			
-			# Convert the dictionaries to JSON and print.
-			self.response.out.write(json.dumps(playlistData))
-		else:
-			error404(self.response)
+		playlistData = []
+		playlistUserMaps = PlaylistUser.gql('WHERE user = :1', user).fetch(limit=None)
+		for playlistUserMap in playlistUserMaps:
+			playlist = Playlist.gql('WHERE playlistId = :1', playlistUserMap.playlistId).get()
+			playlistData.append({
+				'playlistId': playlist.playlistId,
+				'name': playlist.name
+			})
+		
+		# Convert the dictionaries to JSON and output it.
+		self.response.headers['Content-Type'] = 'application/json'
+		self.response.out.write(json.dumps(playlistData))
+
+class PlaylistHandler(webapp.RequestHandler):
+	def get(self, listId):
+		self.response.headers['Content-Type'] = 'application/json'
+		
+		if not listId:
+			errorOut(self.response, 404)
+			return
+		
+		user = users.get_current_user()
+		
+		if not user:
+			errorOut(self.response, 403)
+			return
+		
+		playlist = Playlist.gql('WHERE playlistId = :1', listId).get()
+		
+		if not playlist:
+			errorOut(self.response, 404)
+			return
+		
+		playlistUserMap = PlaylistUser.gql('WHERE user = :1 AND playlistId = :2', user, listId).get()
+		
+		if not playlistUserMap:
+			errorOut(self.response, 403)
+			return
+		
+		songData = []
+		listSongs = PlaylistItem.gql('WHERE playlistId = :1', listId).order('timeAdded').fetch(limit=None)
+		for song in listSongs:
+			songData.append({
+				'title': song.title,
+				'album': song.album,
+				'artist': song.artist
+			})
+		
+		# Convert the dictionaries to JSON and output it.
+		self.response.headers['Content-Type'] = 'application/json'
+		self.response.out.write(json.dumps(playlistData))
 
 class OtherPage(webapp.RequestHandler):
 	def get(self):
-		error404(self.response)
+		errorOut(self.response, 404)
 
 site = webapp.WSGIApplication([('/api/playlists', PlaylistsHandler),
+                               ('/api/playlist/(.*)', PlaylistHandler),
                                ('/.*', OtherPage)],
                               debug=True)
 
